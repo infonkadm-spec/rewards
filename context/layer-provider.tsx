1d0575fc001a9e3e17e2aebce6d8901d9555ff74
infonkadm-spec <infonk.adm@gmail.com>
2025-12-12 00:14:05 -0300

    Update page title to 'Check this out' for white page

--- context/layer-provider.tsx
+++ context/layer-provider.tsx
@@ -30,8 +30,8 @@ export function LayerProvider({
 }: LayerProviderProps) {
 
   // SET LINKS
-  const frontLink = 'https://pay.hotmart.com/M103047413I?off=oklx29sw';
-  const promoLink = 'https://pay.hotmart.com/M103047413I?off=rskh1e73';
+  const frontLink = 'https://pay.hotmart.com/M103047413I?off=oklx29sw&src=v1';
+  const promoLink = 'https://pay.hotmart.com/M103047413I?off=rskh1e73&src=v1';
 
   const contextValue = {
     host,
